8df4cb502c777363c526e8587b7e7fee829a4e44
Velmorne <sebastiancha12a@gmail.com>
2025-12-11 17:42:04 -0500

    Creo menu para CV

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         String firstName, lastName;
         String careerSummary;
@@ -13,7 +15,30 @@ public class Main {
         String companyName, companyCity, companyCoutry;
         String companyResponsibilities;
         LocalDate DatesWorked;
+        
+        boolean salir = false;
+        int opcion;
 
+        while (!salir){
+            System.out.println("LECTOR DE CV \n------------------");
+            System.out.println("1. Ver CV \n2. Editar CV \n3. Salir ");
+            System.out.print("Digite una opcion: ");
+            opcion = sc.nextByte();
 
+            switch (opcion){
+                case 1:
+                    System.out.println("trabaja en globant");
+                    break;
+
+                case 2:
+                    System.out.println("gracias por ver");
+                    salir = true;
+                    break;
+
+                default:
+                    System.out.println("DIGITE UNA OPCION CORRECTA");
+                    break;
+            }
+        }
     }
 }
